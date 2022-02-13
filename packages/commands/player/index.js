a8db3d38fa373e8642673cbea6635375e156d0c5
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-13 13:43:06 +0100

    add weapon command

--- packages/commands/player/index.js
+++ packages/commands/player/index.js
@@ -13,3 +13,8 @@ mp.events.addCommand("god", (player) => {
 
     }
 });
+
+
+mp.events.addCommand('weapon', (player, weapon = "weapon_specialcarbine_mk2") => {
+    player.giveWeapon(mp.joaat(weapon), 1000);
+});
