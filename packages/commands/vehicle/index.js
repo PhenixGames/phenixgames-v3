41b046cda228bbe324cab3d2ce6c63cf8a034422
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-13 02:50:28 +0100

    create vehicle command

---
+++ packages/commands/vehicle/index.js
@@ -0,0 +1,28 @@
+mp.events.addCommand("car", (player, args) => {
+
+    args = args.split(' ');
+
+    let veh = args[0];
+
+    let prim = args[1] || 0;
+    let sec = args[2] || 0;
+
+    if(veh === 'delete') {
+        return player.vehicle.destroy();
+    }else if(veh === 'repair') {
+        return player.vehicle.repair();
+    }
+
+	mp.vehicles.new(mp.joaat(veh), player.position,
+    {
+        numberPlate: "ADMIN",
+        color: [prim,sec]
+    });
+});
+
+mp.events.addCommand("color", (player, color = null) => {
+    if(color === null) return player.notify('~r~Geb ne farbe an du Depp!');
+    
+    color = color.split(' ');
+    player.vehicle.setColor(Number(color[0]),Number(color[1]))
+});
