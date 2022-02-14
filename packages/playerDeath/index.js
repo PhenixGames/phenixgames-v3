0f53ee4314ef38483183eb13c30e06a594afeb3c
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-14 21:41:29 +0100

    Update index.js

--- packages/playerDeath/index.js
+++ packages/playerDeath/index.js
@@ -1,6 +1,8 @@
 function playerDeathHandler(player, reason, killer) {
-    player.health = 100
-    player.spawn(player.position);
+    if(player.getVariable('isTeam')) {
+        player.health = 100
+        player.spawn(player.position);
+    }
  }
  
  mp.events.add("playerDeath", playerDeathHandler);
