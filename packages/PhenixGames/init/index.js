6538f610701bc679992961925abb8eae463dae09
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-14 21:38:37 +0100

    added "setVariable" + save important data into it

--- packages/PhenixGames/init/index.js
+++ packages/PhenixGames/init/index.js
@@ -4,16 +4,19 @@ const database = new Database();
 mp.events.add('playerJoin', async (player) => {
     player.name = player.socialClub;
 
-    await database.query(`SELECT id FROM pg_users WHERE username = ?`, [player.name]).then(res => {
+    await database.query(`SELECT * FROM pg_users WHERE username = ?`, [player.name]).then(res => {
         if(res.length <= 0) {
             //! TO-DO übergeben an LoginScreen, dass User kein Account hat
             return console.log(`[SERVER]: [Not-Registered] ${player.name} has joined the server!`);
         }
+        player.setVariable('playerId', res[0].id);
+        player.setVariable('isTeam', res[0].isTeam);
+        player.setVariable('coins', res[0].coins);
+
         return console.log(`[SERVER]: [Registered] ${player.name} has joined the server!`);
     }).catch(err => {
         console.log(err)
     });
-
 
     var title = 'Spielt auf PhenixGames V3';
     var playing = `Spielt als ${player.name}`;
