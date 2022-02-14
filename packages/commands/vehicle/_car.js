9b8e952d4989cd385120714d081bce534eb19444
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-14 21:38:08 +0100

    added delete car when player already have one + "isTeam" only command

--- packages/commands/vehicle/_car.js
+++ packages/commands/vehicle/_car.js
@@ -1,21 +1,28 @@
 mp.events.addCommand("car", (player, args) => {
+    if(player.getVariable('isTeam')) {
+        args = args.split(' ');
 
-    args = args.split(' ');
+        let veh = args[0];
 
-    let veh = args[0];
+        let prim = args[1] || 0;
+        let sec = args[2] || 0;
 
-    let prim = args[1] || 0;
-    let sec = args[2] || 0;
+        if(veh === 'delete') {
+            return player.vehicle.destroy();
+        }else if(veh === 'repair') {
+            return player.vehicle.repair();
+        }
 
-    if(veh === 'delete') {
-        return player.vehicle.destroy();
-    }else if(veh === 'repair') {
-        return player.vehicle.repair();
+        if(player.getVariable('Player.Tmp.Admin.Veh')) {
+            player.getVariable('Player.Tmp.Admin.Veh').destroy();
+        }
+
+        var setVeh = mp.vehicles.new(mp.joaat(veh), player.position,
+        {
+            numberPlate: "ADMIN",
+            color: [prim,sec]
+        });
+        player.setVariable('Player.Tmp.Admin.Veh', setVeh);
+        player.putIntoVehicle(setVeh, 0);
     }
-
-	mp.vehicles.new(mp.joaat(veh), player.position,
-    {
-        numberPlate: "ADMIN",
-        color: [prim,sec]
-    });
 });
