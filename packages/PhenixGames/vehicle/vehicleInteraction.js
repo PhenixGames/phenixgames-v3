e836bd668a7651040b6764c9ddcb4ca08a61fad7
Ghoster599 <david1235456@gmail.com>
2022-02-20 00:57:22 +0100

    Removed function and replaced with event

--- packages/PhenixGames/vehicle/vehicleInteraction.js
+++ packages/PhenixGames/vehicle/vehicleInteraction.js
@@ -1,7 +1,8 @@
 mp.events.add('keypress:X', (player) => {
-    player.vehicle.engine = true
+    player.vehicle.engine = true;
 });
 
-mp.events.add('playerEnterVehicle', (player, vehicle, seat) => {
-    vehicle.engine = false;
+
+mp.events.add('Vehicle:Engin:state', (player, state) => {
+    player.vehicle.engine = state;
 });
