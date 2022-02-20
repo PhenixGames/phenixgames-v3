b94f2046e324742f4d77f39fc60286c0dfd6524e
Ghoster599 <david1235456@gmail.com>
2022-02-20 15:53:52 +0100

    HotFix

--- packages/PhenixGames/playerDeath/index.js
+++ packages/PhenixGames/playerDeath/index.js
@@ -1,7 +1,7 @@
-mp.events.add("PlayerDeath", (player, reason, killer) => {
+mp.events.add("playerDeath", (player, reason, killer) => {
     console.log("Player Dead");
     player.call("Open:Death:Browser");
-})
+});
 
  mp.events.add("Respawn:At:Hospital", (player) => {
     player.spawn(mp.Vector3(342.44623, -1398.0957, 32.50924));
