2b19f8df630c65d19694ae41fecc32141f5c1578
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-20 12:06:38 +0100

    added automatically require serverside packages

--- packages/PhenixGames/index.js
+++ packages/PhenixGames/index.js
@@ -1,5 +1,5 @@
-require('./account/')
-require('./init/');
-require('./vehicle/');
-require('./houseAPI/');
-require('./markerAPI/');
+const fs = require('fs')
+let modules = fs.readdirSync(__dirname);
+modules.forEach((module) => {
+    require(`${__dirname}/${module}`);
+});
