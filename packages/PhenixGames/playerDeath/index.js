eddbea1af7ad338b585427b021e5852803ff97f3
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-20 16:00:35 +0100

    bug fix

--- packages/PhenixGames/playerDeath/index.js
+++ packages/PhenixGames/playerDeath/index.js
@@ -4,7 +4,7 @@ mp.events.add("playerDeath", (player, reason, killer) => {
 });
 
  mp.events.add("Respawn:At:Hospital", (player) => {
-    player.spawn(mp.Vector3(342.44623, -1398.0957, 32.50924));
+    player.spawn(new mp.Vector3(342.44623, -1398.0957, 32.50924));
     player.notify("~g~Du wurdest Respawnt")
     player.call('close:Death:Browser')
 });
