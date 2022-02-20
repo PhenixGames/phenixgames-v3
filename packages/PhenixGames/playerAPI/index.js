aa4eaf8b92b623c768bc068e9a1a8fc29cb56a5d
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-20 02:28:35 +0100

    improvements
    - added comments
    - finished todo
    - added "changePlayerPos" function to playerAPI
    - code formation

--- packages/PhenixGames/playerAPI/index.js
+++ packages/PhenixGames/playerAPI/index.js
@@ -1,10 +1,19 @@
-const { Database } = require("../../_db/db");
+const {
+    Database
+} = require("../../_db/db");
 const database = new Database();
 
+const config = require('../../../_assets/json/config.json')
+
+/**
+ * 
+ * @param {string} username 
+ * @returns 
+ */
 module.exports.getPlayerId = async function (username) {
     return await database.query('SELECT id FROM pg_users WHERE username = ? LIMIT 1', [username])
         .then(res => {
-            if(res.length > 0) {
+            if (res.length > 0) {
                 return res[0].id
             }
             return false;
@@ -15,31 +24,86 @@ module.exports.getPlayerId = async function (username) {
 }
 
 
+/**
+ * 
+ * @param {string} username 
+ * @param {string} password 
+ * @returns 
+ */
 module.exports.saveNewPlayer = async function (username, password) {
     return await database.query('INSERT INTO pg_users (username, password) VALUES (?, ?)', [username, password])
-        .then(() => {return true})
+        .then(() => {
+            return true
+        })
         .catch(err => {
             console.log(err);
             return false;
         });
 }
 
+/**
+ * 
+ * @param {int} player_id 
+ * @param {string} player_pos 
+ * @returns 
+ */
 module.exports.saveNewPlayerPos = async function (player_id, player_pos) {
     return await database.query('UPDATE pg_users SET last_pos = ? WHERE id = ?', [player_pos, player_id])
-        .then(() => {return true})
+        .then(() => {
+            return true
+        })
         .catch(err => {
             console.log(err);
             return false;
         })
 }
 
+/**
+ * 
+ * @param {int} player_id 
+ * @returns 
+ */
 module.exports.getLastPlayerPos = async function (player_id) {
     return await database.query('SELECT last_pos FROM pg_users WHERE id = ?', [player_id])
         .then(res => {
-            if(res.length > 0) {
+            if (res.length > 0 && res[0].pos != '{"x":"0","y":"0","z":"0"}') {
                 return JSON.parse(res[0].last_pos)
-            }else {
-                //!STANDARD LOCATION
+            } else {
+                return this.changePlayerPos(player, config.defaultSpawn.pos, config.defaultSpawn.rot)
             }
         });
+}
+
+/**
+ * 
+ * @param {object} player 
+ * @param {string} new_pos 
+ * @param {string} new_rot @optional
+ * @returns /
+ */
+module.exports.changePlayerPos = async function (player, new_pos, new_rot) {
+    try {
+        new_pos = new_pos.split(', ');
+        player.position = new mp.Vector3(Number(new_pos[0]), Number(new_pos[1]), Number(new_pos[2]));
+
+        if (new_rot) player.heading = Number(new_rot);
+    } catch (err) {
+        console.log(err);
+    }
+    return;
+}
+
+/**
+ * 
+ * @param {object} player 
+ * @param {object} data 
+ * @returns /
+ */
+module.exports.saveLocalPlayerVar = async function (player, data) {
+
+    for (const [index, [key, value]] of Object.entries(Object.entries(data))) {
+        //console.log(`${index}: ${key} = ${value}`);
+        player.setVariable(key, value);
+    }
+    return;
 }
