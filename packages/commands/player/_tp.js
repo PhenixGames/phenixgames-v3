637002a62a0788979e5480138165255c50bf88d1
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-22 22:30:53 +0100

    bug fix

--- packages/commands/player/_tp.js
+++ packages/commands/player/_tp.js
@@ -3,7 +3,7 @@ const playerAPI = require("../../PhenixGames/playerAPI");
 mp.events.addCommand('tp', (player, location) => {
     if(player.getVariable('isTeam') && location.indexOf(',') !== -1) {
         const changePos = playerAPI.changePlayerPos(player, JSON.stringify(location));
-        if(!changePos) player.notfiy('Etwas ist schief gelaufen!')
+        if(!changePos) player.notify('Etwas ist schief gelaufen!')
     }
 });
 
@@ -14,9 +14,9 @@ mp.events.addCommand('tpto', (player, target) =>{
                 if(tg.socialClub == target){
                     const pos = tg.position.x + ', ' + tg.position.y + ', ' + tg.position.z;
                     const changePos = playerAPI.changePlayerPos(player, pos, null, JSON.stringify(tg.dimension))
-                    if(!changePos) player.notfiy('Etwas ist schief gelaufen!')
+                    if(!changePos) player.notify('Etwas ist schief gelaufen!')
                 }else {
-                    return player.notfiy("Der Spieler wurde nicht gefunden");
+                    return player.notify("Der Spieler wurde nicht gefunden");
                 }
             }
         );
