934c80e7e37c515fbf2548d5f36728a62a8148da
Ghoster599 <david1235456@gmail.com>
2022-02-22 22:18:05 +0100

    Update _tp.js

--- packages/commands/player/_tp.js
+++ packages/commands/player/_tp.js
@@ -15,6 +15,8 @@ mp.events.addCommand('tpto', (player, target) =>{
                     const pos = tg.position.x + ', ' + tg.position.y + ', ' + tg.position.z;
                     const changePos = playerAPI.changePlayerPos(player, pos, null, JSON.stringify(tg.dimension))
                     if(!changePos) player.notfiy('Etwas ist schief gelaufen!')
+                }else {
+                    return player.notfiy("Der Spieler wurde nicht gefunden");
                 }
             }
         );
