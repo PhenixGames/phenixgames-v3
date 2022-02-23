95e86bf69c1ffdbc4f2756ab126501b5b847f1e8
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-23 20:04:16 +0100

    Update index.js

--- packages/PhenixGames/Admin/index.js
+++ packages/PhenixGames/Admin/index.js
@@ -28,14 +28,6 @@ mp.events.addCommand("aduty", async (player) => {
         player.setClothes(8, 122, 0, 0)
         player.setClothes(11, 171, 0, 2)
         player.setClothes(3, 1, 0, 0)
-
-        
-
-        
-       
-
-
-
         player.notify((player.getVariable('Aduty')) ? '~g~Du hast den AdminDiesnt Angetreten': '~r~Du hast den AdminDienst verlassen');
     }
 });
