9261d06f1cd1883b53cc3e48164afc23330170cd
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-23 21:24:52 +0100

    typo

--- packages/PhenixGames/Admin/index.js
+++ packages/PhenixGames/Admin/index.js
@@ -28,7 +28,7 @@ mp.events.addCommand("aduty", async (player) => {
         player.setClothes(8, 122, 0, 0)
         player.setClothes(11, 171, 0, 2)
         player.setClothes(3, 1, 0, 0)
-        player.notify((player.getVariable('Aduty')) ? '~g~Du hast den AdminDiesnt Angetreten': '~r~Du hast den AdminDienst verlassen');
+        player.notify((player.getVariable('Aduty')) ? '~g~Du hast den Admindienst Angetreten': '~r~Du hast den AdminDienst verlassen');
     }
 });
 
