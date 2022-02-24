fa8fc73e949dab454349e0a7f0123af79552a108
Ghoster599 <david1235456@gmail.com>
2022-02-24 19:14:28 +0100

    Added Render for Car details

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -1,6 +1,31 @@
 var Admin = false;
-const player = mp.local.player
 
-mp.events.add("Change:Admin:Value:On:Client", (state) => {
+mp.events.add("Change:Admin:Duty:Value:On:Client", (state) => {
    Admin = state;
+});
+//Render For Car
+mp.events.add("render", () => {
+    if(Admin)
+    {
+        mp.vehicles.forEachInStreamRange((vehicle) => { 
+            if(mp.players.local.position.subtract(vehicle.position).length() < 10)
+            {
+                const drawPosition = [vehicle.position.x, vehicle.position.y, vehicle.position.z + 0.3];
+                mp.game.graphics.drawText(`~b~Id: ~w~${vehicle.remoteId}\n~b~Model: ~w~${mp.game.ui.getLabelText(mp.game.vehicle.getDisplayNameFromVehicleModel(vehicle.model))}\n~b~Position: ~w~${vehicle.position.x.toFixed(2)}, ${vehicle.position.y.toFixed(2)}, ${vehicle.position.z.toFixed(2)}\n`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                });
+                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                });
+            }
+        }); 
+    }
 });
