3abc03f9e22d7f439edffb00b8838e5fad375179
Ghoster599 <david1235456@gmail.com>
2022-02-24 20:42:33 +0100

    Design change

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -61,13 +61,13 @@ mp.events.add("render", () => {
     mp.players.forEachInStreamRange((player) => {
         if(player.getVariable("Aduty")){
             if(mp.players.local.position.subtract(player.position).length() <10 && mp.players.local.position.subtract(player.position).length() !== 0){
-            const drawPosition = [player.position.x, player.position.y, player.position.z + 2];
+            const drawPosition = [player.position.x, player.position.y, player.position.z + 1.2];
             mp.game.graphics.drawText(`${player.name}`, drawPosition, { 
                 font: 0, 
                 color: [255, 255, 255, 185], 
-                scale: [0.25, 0.25], 
-                outline: true,
-                centre: false
+                scale: [1, 1], 
+                outline: false,
+                centre: true
         });
         }
         
