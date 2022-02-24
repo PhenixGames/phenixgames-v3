ce423a748d10922b03c7a3253d39054d43613757
Ghoster599 <david1235456@gmail.com>
2022-02-24 19:30:10 +0100

    Added Speed and Player support

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -2,8 +2,8 @@ var Admin = false;
 
 mp.events.add("Change:Admin:Duty:Value:On:Client", (state) => {
    Admin = state;
-   mp.gui.chat.push("state: " + Admin);
 });
+
 //Render For Car
 mp.events.add("render", () => {
 
@@ -19,8 +19,10 @@ mp.events.add("render", () => {
                     scale: [0.25, 0.25], 
                     outline: true,
                     centre: false
-                });
-                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}`, drawPosition, { 
+                }); 
+                var speed = vehicle.getSpeed();
+                speed = speed * 3.6;
+                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}\n~b~Speed: ~w~${Math.round(speed)}`, drawPosition, { 
                     font: 0, 
                     color: [255, 255, 255, 185], 
                     scale: [0.25, 0.25], 
@@ -29,5 +31,26 @@ mp.events.add("render", () => {
                 });
             }
         }); 
+
+        mp.player.forEachInStreamRange((player) => {
+            if(mp.players.local.position.subtract(player.position).length() <10){
+                const drawPosition = [player.position.x, player.position.y, player.position.z + 0.3];
+                mp.game.graphics.drawText(`~b~Id: ~w~${"Need To be added"}\n~b~Name: ~w~${"Need To be added"}\n~b~Position: ~w~${player.position.x.toFixed(2)}, ${player.position.y.toFixed(2)}, ${player.position.z.toFixed(2)}\n`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                });
+               
+                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${player.getHeading().toFixed(2)}\n~b~Health: ~w~${player.getHealth()}`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                });
+            }
+        });
     }
 });
