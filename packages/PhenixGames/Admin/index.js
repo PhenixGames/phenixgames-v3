46ef08da8f15b4b6539f1195d3b02dd43bee926f
Ghoster599 <david1235456@gmail.com>
2022-02-24 01:04:26 +0100

    Updated Admin Duty - Perms

--- packages/PhenixGames/Admin/index.js
+++ packages/PhenixGames/Admin/index.js
@@ -1,25 +1,29 @@
 const playerAPI = require("../playerAPI");
-
+const Perms = require('../playerAPI/permissionSystem.js')
 const AdminUnsichtbar = 150;
 
 mp.events.addCommand("aduty", async (player) => {
 
-    if(player.getVariable('isTeam')) {
+    if(Perms.hasPermissions(player, ["isTeam"])) {
         //? IF USER IS ALREADY IN GODMODE
-        if(player.getVariable('Aduty')) {
-            playerAPI.saveLocalPlayerVar(player, {'Aduty': false})
-            player.call("Set:God", [false]);
-            var name = await playerAPI.getPlayerInGame(player);
-            player.name = "~r~" + name.firstname + " " + name.lastname;
-            player.alpha = 255;
-        }else {
-            playerAPI.saveLocalPlayerVar(player, {'Aduty': true})
-            player.call("Set:God", [true]); 
-            player.alpha = AdminUnsichtbar;
-            player.name = "~r~" + player.name;
+        if(player.hasPermissions(player, ["godmode"])){
+            if(player.getVariable('Aduty')) {
+                playerAPI.saveLocalPlayerVar(player, {'Aduty': false})
+                player.call("Set:God", [false]);
+                var name = await playerAPI.getPlayerInGame(player);
+                player.name = "~r~" + name.firstname + " " + name.lastname;
+                player.alpha = 255;
+            }else {
+                playerAPI.saveLocalPlayerVar(player, {'Aduty': true})
+                player.call("Set:God", [true]); 
+                player.alpha = AdminUnsichtbar;
+                player.name = "~r~" + player.name;
+            }
         }
-        player.call("Player:Admin:Duty:noclip");
-
+        if(player.hasPermissions(player, ["no_clip"])){
+            player.call("Player:Admin:Duty:noclip");
+        }
+        
         //!TODO Remove when own cloth system is implemented
         player.setClothes(1, 130, 0, 2)
         player.setClothes(4, 34, 0, 2)
@@ -28,7 +32,7 @@ mp.events.addCommand("aduty", async (player) => {
         player.setClothes(8, 122, 0, 0)
         player.setClothes(11, 171, 0, 2)
         player.setClothes(3, 1, 0, 0)
-        player.notify((player.getVariable('Aduty')) ? '~g~Du hast den Admindienst Angetreten': '~r~Du hast den AdminDienst verlassen');
+        player.notify((player.getVariable('Aduty')) ? '~g~Du hast den Admindienst als '+  Perms.getRoleInfo(player) + ' Angetreten': '~r~Du hast den AdminDienst verlassen');
     }
 });
 
