882ff4d7c6f3967fdb40002e3a66d58dbd78635d
Ghoster599 <david1235456@gmail.com>
2022-02-26 23:42:26 +0100

    Update index.js

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -10,26 +10,28 @@ mp.events.add("render", () => {
     if(Admin)
     {
         mp.vehicles.forEachInStreamRange((vehicle) => { 
-            if(mp.players.local.position.subtract(vehicle.position).length() < 10 && mp.players.local.vehicle.position.subtract(player.position).length() !== 0)
+            if(mp.players.local.position.subtract(vehicle.position).length() < 10)
             {
+                if(mp.players.local.vehicle == vehicle) {
+                    mp.players.local.notify("debug XDDDD");
+                }
                 const drawPosition = [vehicle.position.x, vehicle.position.y, vehicle.position.z + 0.3];
-                    mp.game.graphics.drawText(`~b~Id: ~w~${vehicle.getVariable('veh_id')}\n~b~Model: ~w~${mp.game.ui.getLabelText(mp.game.vehicle.getDisplayNameFromVehicleModel(vehicle.model))}\n~b~Position: ~w~${vehicle.position.x.toFixed(2)}, ${vehicle.position.y.toFixed(2)}, ${vehicle.position.z.toFixed(2)}\n`, drawPosition, { 
-                        font: 0, 
-                        color: [255, 255, 255, 185], 
-                        scale: [0.25, 0.25], 
-                        outline: true,
-                        centre: false
-                    }); 
+                mp.game.graphics.drawText(`~b~Id: ~w~${vehicle.getVariable('veh_id')}\n~b~Model: ~w~${mp.game.ui.getLabelText(mp.game.vehicle.getDisplayNameFromVehicleModel(vehicle.model))}\n~b~Position: ~w~${vehicle.position.x.toFixed(2)}, ${vehicle.position.y.toFixed(2)}, ${vehicle.position.z.toFixed(2)}\n`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                }); 
                 var speed = vehicle.getSpeed();
                 speed = speed * 3.6;
-                    mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}\n~b~Speed: ~w~${Math.round(speed)}\n`, drawPosition, { 
-                        font: 0, 
-                        color: [255, 255, 255, 185], 
-                        scale: [0.25, 0.25], 
-                        outline: true,
-                        centre: false
-                    });
-               
+                mp.game.graphics.drawText(`\n\n\n~b~Heading: ~w~${vehicle.getHeading().toFixed(2)}\n~b~Health: ~w~${vehicle.getHealth()}\n~b~Speed: ~w~${Math.round(speed)}\n`, drawPosition, { 
+                    font: 0, 
+                    color: [255, 255, 255, 185], 
+                    scale: [0.25, 0.25], 
+                    outline: true,
+                    centre: false
+                });
             }
         }); 
 
