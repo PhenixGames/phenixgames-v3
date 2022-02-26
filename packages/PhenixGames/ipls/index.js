eb4640274a867eecf4837556618966334ecf9edb
Ghoster599 <david1235456@gmail.com>
2022-02-27 00:32:29 +0100

    Added IPLS

--- packages/PhenixGames/ipls/index.js
+++ packages/PhenixGames/ipls/index.js
@@ -1,6 +1,8 @@
 let Ipls = [
     'vw_casino_main',
-    'vw_casino_penthouse'
+    'vw_casino_penthouse',
+    'vw_casino_carpark',
+    'vw_casino_garage'
 ]
 
 mp.events.add('packagesLoaded', () =>
