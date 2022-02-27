0687b86ed5b42eb1caefc008005d6866abfdf147
Ghoster599 <david1235456@gmail.com>
2022-02-27 20:25:09 +0100

    Update index.js

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -130,7 +130,7 @@ mp.events.add("render", () => {
 });
 
 //Dieses Event ändert die Variable auf den Passenden wert der vom server übergeben wurde
-mp.events.add("Set:ModdedSpeed", (Speed) => {
+mp.events.add("Set:ModdedSpeed", (speed) => {
     mp.game.graphics.notify(`Dein Speed wurde auf ${speed} gesetzt`);
-    modded_Speed = Number(Speed);
+    modded_Speed = Number(speed);
 });
