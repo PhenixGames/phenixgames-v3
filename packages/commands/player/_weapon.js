684a34eddeba89d41662eac374388a506aaacdb7
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 00:25:43 +0100

    fixed bug 	modified:   packages/commands/player/_heal.js 	modified:   packages/commands/player/_tp.js 	modified:   packages/commands/player/_weapon.js

--- packages/commands/player/_weapon.js
+++ packages/commands/player/_weapon.js
@@ -1,6 +1,6 @@
 const Perms = require('../../PhenixGames/playerAPI/permissionSystem.js')
-mp.events.addCommand('weapon', (player, weapon = "specialcarbine_mk2") => {
-    if(Perms.hasPermissions(player, ["spawn_weapon"])) {
+mp.events.addCommand('weapon', async (player, weapon = "specialcarbine_mk2") => {
+    if(await Perms.hasPermissions(player, ["spawn_weapon"])) {
         player.giveWeapon(mp.joaat("weapon_" + weapon), 10000);
     }
 });
