889bc63a991af0d1ba46d2f806249d09d81eeb1d
Ghoster599 <david1235456@gmail.com>
2022-03-01 00:51:17 +0100

    Create index.js

---
+++ client_packages/index.js
@@ -0,0 +1,17 @@
+require('./gui/login/');
+require('./gui/spawnChoose/');
+require('./gui/deathscreen');
+require('./gui/register/namechooser/');
+require('./gui/money/');
+require('./gui/HUD/')
+
+
+require('./non-gui/godmode/')
+require('./non-gui/discord/');
+require('./non-gui/vehicle/');
+require('./non-gui/marker/');
+require('./non-gui/noclip/');
+require('./non-gui/fingerpoint/');
+require('./non-gui/player/');
+require('./non-gui/voice/');
+require('./non-gui/damage/');
