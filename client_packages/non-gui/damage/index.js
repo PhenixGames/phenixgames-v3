5e3ef19ae55816b5cb680170597e09feed46f583
Ghoster599 <david1235456@gmail.com>
2022-03-01 00:50:45 +0100

    Create index.js

---
+++ client_packages/non-gui/damage/index.js
@@ -0,0 +1,21 @@
+mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapon, boneIndex, damage) => {
+    if (targetEntity.type === 'player') {
+        var newdamage;
+        let weaponHash = mp.game.invoke(`0x0A6DB4965674D243`, mp.players.local.handle); //GET_SELECTED_PED_WEAPON
+        let weaponModel = mp.game.weapon.getWeapontypeModel(weaponHash);
+
+
+
+
+
+        
+        newdamage = damage
+
+
+        mp.gui.chat.push(`hash: ${weaponHash}, model: ${weaponModel}`);
+        mp.gui.chat.push(`olddmg: ${damage}, newdmg: ${newdamage}`);
+        mp.events.callRemote("Apply:Damage:to:Player", targetEntity, newdamage);
+    }else {
+        return true;
+    }
+});
