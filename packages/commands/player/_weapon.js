5cd2aa99c4c38dacf83cdafd1ab1dfe03c358304
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 00:29:16 +0100

    fixed bug 	modified:   packages/PhenixGames/admin/index.js 	modified:   packages/commands/player/_weapon.js

--- packages/commands/player/_weapon.js
+++ packages/commands/player/_weapon.js
@@ -1,4 +1,5 @@
-const Perms = require('../../PhenixGames/playerAPI/permissionSystem.js')
+const Perms = require('../../PhenixGames/playerAPI/permissionSystem.js');
+
 mp.events.addCommand('weapon', async (player, weapon = "specialcarbine_mk2") => {
     if(await Perms.hasPermissions(player, ["spawn_weapon"])) {
         player.giveWeapon(mp.joaat("weapon_" + weapon), 10000);
