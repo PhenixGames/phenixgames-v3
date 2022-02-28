002888095d451e775e0950e77f8f55cd4437d3dd
Ghoster599 <david1235456@gmail.com>
2022-03-01 00:16:38 +0100

    Update index.js

--- client_packages/gui/HUD/index.js
+++ client_packages/gui/HUD/index.js
@@ -139,6 +139,4 @@ mp.keys.bind(0x73, true, function() {//F4
     mp.voiceChat.muted = !mp.voiceChat.muted;
     if(!mp.voiceChat.muted) browser.execute(`manageVoice("${0}");`);
     else browser.execute(`manageVoice("${1}");`);
-    
-    //mp.game.graphics.notify("Voice Chat: " + ((!mp.voiceChat.muted) ? "~g~enabled" : "~r~disabled"));
 });
