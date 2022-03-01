abf4d8e6ef1b12a87604bef176cf8d77b85f16f0
Ghoster599 <david1235456@gmail.com>
2022-03-01 01:38:21 +0100

    Added Heatschotmultiplikator

--- client_packages/non-gui/damage/index.js
+++ client_packages/non-gui/damage/index.js
@@ -1,15 +1,25 @@
 mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapon, boneIndex, damage) => {
     if (targetEntity.type === 'player') {
         var newdamage;
+        const Headshotmp = 1.1;
         let weaponHash = mp.game.invoke(`0x0A6DB4965674D243`, mp.players.local.handle); //GET_SELECTED_PED_WEAPON
         let weaponModel = mp.game.weapon.getWeapontypeModel(weaponHash);
+        const Headshot = false;
 
-
-
+        if (boneIndex === 20) {
+            Headshot = true;
+        }
+       
 
 
         
         newdamage = damage * 11;
+
+
+        //Headshotmp ist am ende auf den dmg wert anzuwenden falls ein Headschot gemacht wurde!
+        if(Headshot){
+            newdamage = newdamage * Headshotmp;
+        }
 
 
         mp.gui.chat.push(`hash: ${weaponHash}, model: ${weaponModel}`);
