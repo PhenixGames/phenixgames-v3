30d43d9ae5ecdf100eae467a8f8a59c401335e95
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 01:11:21 +0100

    modified:   packages/PhenixGames/playerAPI/_events.js

--- packages/PhenixGames/playerAPI/_events.js
+++ packages/PhenixGames/playerAPI/_events.js
@@ -1,3 +1,11 @@
 mp.events.add('Apply:Damage:to:Player', (player, target, damage) => {
-    target.health = player.health - damage;
+    target.health = target.health - damage;
+
+    const health = (target.health < 0) ? 0 : target.health;
+
+    console.log(health);
+
+    setTimeout(() => {
+        console.log('target new healt ' + health)
+    }, 100)
 });
