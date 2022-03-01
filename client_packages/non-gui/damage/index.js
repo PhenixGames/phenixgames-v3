fe62f36fceda862f6a84aa988e45aa75f55a7f08
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 01:41:35 +0100

    modified:   client_packages/non-gui/damage/index.js

--- client_packages/non-gui/damage/index.js
+++ client_packages/non-gui/damage/index.js
@@ -4,7 +4,7 @@ mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapo
         const Headshotmp = 1.1;
         let weaponHash = mp.game.invoke(`0x0A6DB4965674D243`, mp.players.local.handle); //GET_SELECTED_PED_WEAPON
         let weaponModel = mp.game.weapon.getWeapontypeModel(weaponHash);
-        const Headshot = false;
+        var Headshot = false;
 
         if (boneIndex === 20) {
             Headshot = true;
@@ -12,7 +12,7 @@ mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapo
        
 
 
-        
+  
         newdamage = damage * 11;
 
 
