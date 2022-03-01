2428ff2007f13d5500cfc7db41a07d2c733224b8
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-01 01:32:41 +0100

    bug fix 	modified:   packages/PhenixGames/playerAPI/_events.js

--- packages/PhenixGames/playerAPI/_events.js
+++ packages/PhenixGames/playerAPI/_events.js
@@ -3,14 +3,19 @@ mp.events.add('Apply:Damage:to:Player', (player, target, damage) => {
     var health = target.health;
     var armour = target.armour;
 
-    if(damage > armour){
-        damage = damage - armour
+    console.log(health, armour)
+
+    if(damage >= armour){
+        damage = damage - armour;
+
         target.armour = 0;
+
         if(health < damage){
-            target.healt = 0;
+            target.health = 0;
         }else {
-            target.healt = health - damage;
+            target.health = health - damage;
         }
+
     }else {
         target.armour = armour - damage;
     }
