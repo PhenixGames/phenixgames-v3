cd45fea3c6835b720f287e8a2b7c9302f74e33e7
Ghoster599 <david1235456@gmail.com>
2022-03-05 00:18:00 +0100

    removed stupid cloth

--- packages/PhenixGames/admin/index.js
+++ packages/PhenixGames/admin/index.js
@@ -33,14 +33,6 @@ mp.events.addCommand("aduty", async (player) => {
 
         
         
-        //!TODO Remove when own cloth system is implemented
-        player.setClothes(1, 130, 0, 2)
-        player.setClothes(4, 34, 0, 2)
-        player.setClothes(6, 60, 0, 2)
-        player.setClothes(7, 147, 0, 2)
-        player.setClothes(8, 122, 0, 0)
-        player.setClothes(11, 171, 0, 2)
-        player.setClothes(3, 1, 0, 0)
 
         var rolename = await Perms.getRoleInfo(player);
 
