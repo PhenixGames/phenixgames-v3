c72dce2939f2eff13e5469fb7cfe0840ab8deece
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-04 20:03:45 +0100

    Update index.js

--- client_packages/non-gui/damage/index.js
+++ client_packages/non-gui/damage/index.js
@@ -1,4 +1,4 @@
-const damageConfig = require('package://non-gui/damage/damage.json');
+const damageConfig = require('package://[...]/damage.json');
 
 mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapon, boneIndex, damage) => {
     if (targetEntity.type === 'player') {
