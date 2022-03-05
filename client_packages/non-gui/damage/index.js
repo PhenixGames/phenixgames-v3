0389515ba68423321e3a9e4722e59a4768d14923
Ghoster599 <david1235456@gmail.com>
2022-03-05 20:48:27 +0100

    Update index.js

--- client_packages/non-gui/damage/index.js
+++ client_packages/non-gui/damage/index.js
@@ -19,5 +19,12 @@ mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
 });
 mp.events.add('render', () => {
     mp.game.controls.disableControlAction(0, 140, true);
-    mp.game.controls.disableControlAction(0, 142, true);
 });
+
+
+//Debug
+ mp.keys.bind(0x76, false, function () { // F7 key
+let weaponHash = mp.game.invoke(`0x0A6DB4965674D243`, mp.players.local.handle); //GET_SELECTED_PED_WEAPON
+let weaponModel = mp.game.weapon.getWeapontypeModel(weaponHash);
+mp.gui.chat.push(`hash: ${weaponHash}, model: ${weaponModel}`);
+})
