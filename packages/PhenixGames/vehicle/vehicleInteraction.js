e3740d211d1ade138e0026a9498c222647cf5e96
Ghoster599 <david1235456@gmail.com>
2022-03-05 21:25:38 +0100

    added Feature - Veh engin stays on

--- packages/PhenixGames/vehicle/vehicleInteraction.js
+++ packages/PhenixGames/vehicle/vehicleInteraction.js
@@ -1,7 +1,19 @@
 mp.events.add('keypress:X', (player) => {
     try {
-        player.vehicle.engine = !player.vehicle.engine;
+        var veh = player.vehicle;
+        veh.engine = !veh.engine;
+        veh.setVariable("Engin", veh.engine);
     }catch(err) {
         return;
     }
+});
+
+mp.events.add("playerExitVehicle", (player, vehicle) => {
+    setTimeout(() => {
+        try{
+            vehicle.engine = vehicle.getVariable("Engin");
+        }catch(err){
+            return;
+        }
+    }, 1000);
 });
