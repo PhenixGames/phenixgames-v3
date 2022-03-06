1547c13472b655f208b49009b9d72f15f1bd2995
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-06 18:37:13 +0100

    added "permsupdate" command

--- packages/commands/admin/_perms.js
+++ packages/commands/admin/_perms.js
@@ -1,3 +1,13 @@
+const permissionSystem = require('../playerAPI/permissionSystem')
+
 mp.events.add('perms', ( player, args ) => {
-console.log(JSON.stringify(args));
+    console.log(JSON.stringify(args));
+});
+
+mp.events.addCommand("permsupdate", () => {
+    mp.players.forEach((player) => {
+        if(player.getVariable('syncPlayer')) {
+            permissionSystem.setPlayerPermissionsLocal(player);
+        }
+    });
 });
