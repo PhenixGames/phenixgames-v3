2b7cd642401dd989933c53b28d9c05106bee9253
Ghoster599 <david1235456@gmail.com>
2022-03-11 00:05:37 +0100

    Moved Keypresses

---
+++ client_packages/non-gui/keypresser/index.js
@@ -0,0 +1,26 @@
+// -STRG-
+mp.keys.bind(0x11, true, function() {
+    mp.events.callRemote('keypress:X');
+    if(mp.players.local.vehicle.isSirenOn()) {
+        setTimeout(() => {
+            mp.players.local.vehicle.setSiren(true);
+        }, 1);
+        
+    }
+});
+
+// -X-
+mp.keys.bind(0x58, true, function() {
+    mp.players.local.vehicle.setSirenSound((mp.players.local.vehicle.isSirenSoundOn() == 1) ? true : false)
+});
+
+//f2
+mp.keys.bind(0x71, true, function() {
+    mp.events.callRemote('Player:pressed:f2');
+});
+
+//f10
+
+mp.keys.bind(0x79, true, function() {
+    mp.players.local.setToRagdoll(3000, 6000, 0, false, false, false);
+});
--- client_packages/non-gui/vehicle/index.js
+++ client_packages/non-gui/vehicle/index.js
@@ -1,43 +1,17 @@
-// -STRG-
-mp.keys.bind(0x11, true, function() {
-    mp.events.callRemote('keypress:X');
-    if(mp.players.local.vehicle.isSirenOn()) {
-        setTimeout(() => {
-            mp.players.local.vehicle.setSiren(true);
-        }, 1);
-        
-    }
-});
-
-// -X-
-mp.keys.bind(0x58, true, function() {
-    mp.players.local.vehicle.setSirenSound((mp.players.local.vehicle.isSirenSoundOn() == 1) ? true : false)
-});
-
-//f2
-mp.keys.bind(0x71, true, function() {
-    mp.events.callRemote('Player:pressed:f2');
-});
-
-//f10
-
-mp.keys.bind(0x79, true, function() {
-    mp.players.local.setToRagdoll(3000, 6000, 0, false, false, false);
-});
-
 mp.events.add('Vehicle:Engine:state', (state) => {
     if(mp.players.local.vehicle){
         mp.game.vehicle.defaultEngineBehaviour = false;
         setTimeout(() => {
-            mp.players.local.vehicle.engine = state;
+            mp.players.local.vehicle.engine = state; // setzen der Engin
             
         }, 1000)
     }
 
 });
+//ConfigFlag, dass der Motor ausbleibt beim einsteigen.
 mp.players.local.setConfigFlag(429, true);
 
 
 mp.events.add("playerEnterVehicle", (player, vehicle, seat) => {    
-    mp.game.vehicle.defaultEngineBehaviour = false;
+    mp.game.vehicle.defaultEngineBehaviour = false;//enginBehabior, dass der Motor ausbleibt beim einsteigen.
 });
