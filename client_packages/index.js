1e555df72ff553e8d1492210888b5588f1303305
Ghoster599 <david1235456@gmail.com>
2022-03-11 00:05:24 +0100

    Changed Require starter

--- client_packages/index.js
+++ client_packages/index.js
@@ -6,13 +6,11 @@ require('./gui/money/');
 require('./gui/HUD/')
 require('./gui/inventory/')
 
-
-require('./non-gui/godmode/')
 require('./non-gui/discord/');
 require('./non-gui/vehicle/');
-require('./non-gui/marker/');
 require('./non-gui/noclip/');
 require('./non-gui/fingerpoint/');
 require('./non-gui/player/');
 require('./non-gui/voice/');
 require('./non-gui/damage/');
+require('./non-gui/keypresser/');
