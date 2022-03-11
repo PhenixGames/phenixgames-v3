b3d10503154ba212d03c1f940c2d9e6c4bf595d9
Ghoster599 <david1235456@gmail.com>
2022-03-11 01:29:27 +0100

    Added clientside support for Seatbelts

--- client_packages/non-gui/vehicle/index.js
+++ client_packages/non-gui/vehicle/index.js
@@ -15,3 +15,16 @@ mp.players.local.setConfigFlag(429, true);
 mp.events.add("playerEnterVehicle", (player, vehicle, seat) => {    
     mp.game.vehicle.defaultEngineBehaviour = false;//enginBehabior, dass der Motor ausbleibt beim einsteigen.
 });
+
+let seatbelt = false;
+mp.events.add('Apply:SeatBelt', (player) => {
+    player.setConfigFlag(32, !seatbelt);
+    seatbelt = !seatbelt;
+});
+
+mp.events.add('playerLeaveVehicle', ()  => {
+   if(seatbelt){
+    mp.game.graphics.notify('Du hast dich beim aussteigen abgeschnallt');
+    seatbelt = false;
+   }
+});
