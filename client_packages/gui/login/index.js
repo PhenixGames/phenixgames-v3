ae4da8e9c47be6c26229d403caaf870d72df7ad6
Ghoster599 <david1235456@gmail.com>
2022-03-11 18:13:56 +0100

    Update index.js

--- client_packages/gui/login/index.js
+++ client_packages/gui/login/index.js
@@ -1,10 +1,10 @@
-// const mainBrowser = require('index');
+ const mainBrowser = require('index');
 
 var LoginCam;
-// mp.events.add('Open:Login:Browser', () => {
-//     mp.gui.chat.push(JSON.stringify(mainBrowser))
-//     //mainBrowser.execute(`gui.Login.show()`)
-// });
+mp.events.add('Open:Login:Browser', () => {
+    mp.gui.chat.push(JSON.stringify(mainBrowser))
+    //mainBrowser.execute(`gui.Login.show()`)
+});
 
 mp.events.add('Create:Login:Cam', ( ) => {
     LoginCam = mp.cameras.new('default', new mp.Vector3(-100, -966, 296), new mp.Vector3(0,0,-159), 40);
