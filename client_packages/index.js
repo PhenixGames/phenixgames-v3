ccf3014db07ee1524b62b671e4182130cd885d50
Ghoster599 <david1235456@gmail.com>
2022-03-11 17:40:29 +0100

    Update index.js

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,15 +1,14 @@
-// require('./gui/login/');
-// require('./gui/spawnChoose/');
-// require('./gui/deathscreen');
-// require('./gui/register/namechooser/');
-// require('./gui/money/');
-// require('./gui/HUD/')
-// require('./gui/inventory/')
-
+require(`./gui`);
+require('./gui/login/');
+require('./gui/spawnChoose/');
+require('./gui/deathscreen');
+require('./gui/register/namechooser/');
+require('./gui/inventory/')
+require('./gui/money/');
+require('./gui/HUD/');
 
 require('./non-gui/vehicle/');
 require('./non-gui/noclip/');
 require('./non-gui/fingerpoint/');
 require('./non-gui/player/');
 require('./non-gui/voice/');
-require('./non-gui/keypresser/');
