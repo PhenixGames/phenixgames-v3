361e3ad06e9a53853968fd9bbdf25f1efc62a48a
Ghoster599 <david1235456@gmail.com>
2022-03-11 21:54:57 +0100

    added Revive command

--- packages/commands/player/_heal.js
+++ packages/commands/player/_heal.js
@@ -7,3 +7,32 @@ mp.events.addCommand("heal", async (player) => {
     }
 
 });
+
+mp.events.addCommand("revive", async (player, target) => {
+    if(await Perms.hasPermissions(player, ["revive"])) {
+        if(typeof target == 'number'){
+            mp.players.forEach(
+                (tg) => {
+                    if(tg.getVariable("playerId") == target){
+                        tg.cal("close:Death:Browser");
+                        tg.spawn(new mp.Vector3(tg.position.x, tg.position.y, tg.position.z + 1));
+                    }else {
+                        return player.notify(`Der Spieler mit der ID ${target} wurde nicht gefunden`);
+                    }
+                }
+            );
+        }else {
+            mp.players.forEach(
+                (tg) => {
+                    if(tg.socialClub == target){
+                        tg.cal("close:Death:Browser");
+                        tg.spawn(new mp.Vector3(tg.position.x, tg.position.y, tg.position.z + 1));
+                    }else {
+                        return player.notify("Der Spieler wurde nicht gefunden");
+                    }
+                }
+            );
+        }
+    }
+
+});
