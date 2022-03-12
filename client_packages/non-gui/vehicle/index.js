5f0f2b4f558b499529c4234a9647d9b06aeae7d7
Ghoster599 <david1235456@gmail.com>
2022-03-12 19:51:15 +0100

    Update index.js

--- client_packages/non-gui/vehicle/index.js
+++ client_packages/non-gui/vehicle/index.js
@@ -30,7 +30,7 @@ mp.events.add('playerLeaveVehicle', ()  => {
 mp.events.add("Vehicle:Remove:Dirt:Level", (args) =>{
     mp.console.logError("" + JSON.stringify(args), true, true);
     if (args[0].type !== 'vehicle') return;
-    args.setDirtLevel(0);
+    args[0].setDirtLevel(0);
 
 });
 
