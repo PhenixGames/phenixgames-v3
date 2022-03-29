f39656315f622ec5e9bedb4da0d1c82c969959f7
Ghoster599 <david1235456@gmail.com>
2022-03-29 22:06:12 +0200

    Create index.js

---
+++ packages/PhenixGames/Fuelstation/index.js
@@ -0,0 +1,18 @@
+
+//Load all Fuel stations
+//mp.colshapes.newRectangle(x, y, width, height, dimension)
+
+//Test
+//POSITION: -2096.231689453125, -320.1805114746094, 12.16186809539795
+var pos = new vector3(-2096.231689453125, -320.1805114746094, 12.16186809539795);
+var col = mp.colshapes.newCircle(pos.x , pos.y, 20);
+col.setVariable("debuggggg", 1);
+
+mp.events.add('playerEnterColshape', (player, shape) => {
+    player.notify(`You entered a colshape with id "${shape.getVariable("debuggggg")}".`);
+});
+
+mp.events.add("playerExitCheckpoint", (checkpoint) => {
+    console.log(checkpoint);
+    player.notify("Testttt");
+   });
