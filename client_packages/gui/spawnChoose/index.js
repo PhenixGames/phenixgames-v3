72e95e96ef49fef814b14207678f473256831505
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-29 22:13:19 +0200

    fixed login cursor

--- client_packages/gui/spawnChoose/index.js
+++ client_packages/gui/spawnChoose/index.js
@@ -4,11 +4,13 @@ var spawnChooseBrowser;
 
 mp.events.add('Player:Spawn:Options', (type) => {
     mp.players.local.freezePosition(true);
-    mp.gui.cursor.visible = true;
     mp.game.ui.displayRadar(false);
     mp.game.ui.displayHud(false);
     mp.gui.chat.show(false);
     spawnChooseBrowser = mp.browsers.new(`http://${config.domain}:8080/#/spawnchooser`);
+    setTimeout(() => {
+        mp.gui.cursor.show(true, true);
+    }, 500);
 });
 
 mp.events.add('Player:Spawn', (type) => {
@@ -33,7 +35,7 @@ mp.events.add('Player:Spawn', (type) => {
 mp.events.add('Player:Spawn:Succes:close:Windows', () => {
     mp.events.remove(["Player:Spawn", "Player:Spawn:Options", "Player:Spawn:Succes:close:Windows"]);
     spawnChooseBrowser.destroy();
-    mp.gui.cursor.visible = false;
+    mp.gui.cursor.show(false, false);
     mp.players.local.freezePosition(false);
     mp.gui.chat.show(true);
     mp.game.ui.displayRadar(true);
