a2175040470ef8ad4b179b1bb56781e452fc2d73
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-29 20:52:16 +0200

    removed old vue_server

--- vue_server/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style>
-@import "https://blackdayz.github.io/BlackCSS/lib/BlackCSS.css";
-@import "https://fonts.googleapis.com/css?family=Open+Sans";
-</style>
