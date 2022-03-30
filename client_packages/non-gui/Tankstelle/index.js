978895df5547b6f8e578919af747ed6af2765ee1
Ghoster599 <david1235456@gmail.com>
2022-03-30 19:28:52 +0200

    Update index.js

--- client_packages/non-gui/Tankstelle/index.js
+++ client_packages/non-gui/Tankstelle/index.js
@@ -3,7 +3,7 @@
         //-156.15350341796875
         //
         var pos = new mp.Vector3(-2096.231689453125, -320.1805114746094, 12.16186809539795);
-        mp.markers.new(25, pos, 40,
+        mp.markers.new(6, pos, 40,
             {
                 direction: -156.15,
                 rotation: -156.15,
