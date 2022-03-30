1b632d2d6ac16f4985fff12ec856a8a0ace2b4e5
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-30 20:30:10 +0200

    modified:   client_packages/gui/inventory/index.js

--- client_packages/gui/inventory/index.js
+++ client_packages/gui/inventory/index.js
@@ -3,5 +3,6 @@ const config = require('_config/config').config;
 var invBrowser;
 
 mp.events.add('Player:ActivateInventory', () => {
+    mp.console.logInfo('1')
     invBrowser = mp.browsers.new(`http://${config.domain}:8080/#/inventory`);
 });
