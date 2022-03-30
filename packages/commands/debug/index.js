bf4c2b378516c98856c9336e5aecb206b7f1d734
Ghoster599 <david1235456@gmail.com>
2022-03-30 20:15:39 +0200

    Update index.js

--- packages/commands/debug/index.js
+++ packages/commands/debug/index.js
@@ -13,3 +13,11 @@ mp.events.addCommand('spawnped', ( player, arg, ped ) => {
     let dynamicPed = mp.peds.new(mp.joaat(ped), mp.players.at(0).position, {dynamic:true});
 dynamicPed.controller = mp.players.at(0);
  });
+
+ mp.events.addCommand('testtest', (player, args, min, max) => {
+     getRandomInt(min, max);
+    });
+ //Erstelle eine Funkton welche eine random nummer returnt
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
