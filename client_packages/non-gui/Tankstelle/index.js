36086b4a99370a377b0f4eef43eda2e4e284165c
Ghoster599 <david1235456@gmail.com>
2022-03-30 19:24:07 +0200

    Update index.js

--- client_packages/non-gui/Tankstelle/index.js
+++ client_packages/non-gui/Tankstelle/index.js
@@ -1,8 +1,13 @@
     mp.events.add("Create:Tankstellen:Main:Marker", () => {
+        //
+        //-156.15350341796875
+        //
         var pos = new mp.Vector3(-2096.231689453125, -320.1805114746094, 12.16186809539795);
         mp.markers.new(20, pos, 20,
             {
-                color: [0, 0, 0, 255],
+                direction: -156.15,
+                rotation: -156.15,
+                color: [255, 255, 255, 255],
                 visible: true,
                 dimension: 0
             });
