98f6df212007fa367505134bb1f1980e429c0c97
Ghoster599 <david1235456@gmail.com>
2022-04-03 17:45:57 +0200

    Update for Fule station

--- packages/PhenixGames/Fuelstation/index.js
+++ packages/PhenixGames/Fuelstation/index.js
@@ -1,4 +1,9 @@
-
+var Fuelstations = [
+        1,
+        2,
+        3,
+        4,
+]
 //Load all Fuel stations
 //mp.colshapes.newRectangle(x, y, width, height, dimension)
 
@@ -6,14 +11,25 @@
 //POSITION: -2096.231689453125, -320.1805114746094, 12.16186809539795
 var pos = new mp.Vector3(-2096.231689453125, -320.1805114746094, 12.16186809539795);
 var col = mp.colshapes.newCircle(pos.x , pos.y, 20);
-col.setVariable("debuggggg", 1);
+col.setVariable("Type", 1);
 
 mp.events.add('playerEnterColshape', (player, shape) => {
-    player.notify(`You entered a colshape with id "${shape.getVariable("debuggggg")}".`);
-    player.call("Create:Tankstellen:Main:Marker");
+  if(isentfuelstation(player, shape)){
+      player.call("Du betrittst das gelände einer Tankstelle");
+      player.call("fuelstation.open", [shape.getVariable("Type")]);
+  }
 });
 
-mp.events.add("playerExitCheckpoint", (checkpoint) => {
-    console.log(checkpoint);
+mp.events.add("playerExitCheckpoint", (col) => {
+    console.log(col);
     player.notify("Testttt");
    });
+
+module.export.isentfuelstation = async function(player, shape){
+       for (var i = 0; i < Fuelstations.length; i++) {
+          if(Fuelstations[i] == shape.getVariable("Type")){
+              return true
+          }
+       }
+       return false;
+   }
