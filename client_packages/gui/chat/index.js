320a9a8030a877ad4b1c116cf2ede017b33ff398
Mittelblut9 <sauer.benedikt@outlook.de>
2022-05-07 18:58:50 +0200

    modified:   client_packages/gui/chat/index.js

--- client_packages/gui/chat/index.js
+++ client_packages/gui/chat/index.js
@@ -2,7 +2,7 @@
 mp.gui.chat.show(false);
 
 // Initialize chatbox CEF, mark it as default server chat
-const chatbox = mp.browsers.new('package://chat/chat-ui/index.html');
+const chatbox = mp.browsers.new('package://gui/chat/chat-ui/index.html');
 chatbox.markAsChat();
 
 mp.gui.chat.colors = true;
