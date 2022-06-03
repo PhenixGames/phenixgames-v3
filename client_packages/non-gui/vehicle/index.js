eaa0c39068807ec7a121f89ea63848f14050d5e9
Mittelblut9 <sauer.benedikt@outlook.de>
2022-06-03 20:43:47 +0200

    Added: siren dont goes off if player leave veh

--- client_packages/non-gui/vehicle/index.js
+++ client_packages/non-gui/vehicle/index.js
@@ -2,8 +2,7 @@ mp.events.add('Vehicle:Engine:state', (state) => {
     if(mp.players.local.vehicle){
         mp.game.vehicle.defaultEngineBehaviour = false;
         setTimeout(() => {
-            mp.players.local.vehicle.engine = state; // setzen der Engin
-            
+            mp.players.local.vehicle.engine = state; // setzen der Engine
         }, 1000)
     }
 
@@ -14,6 +13,8 @@ mp.players.local.setConfigFlag(429, true);
 
 mp.events.add("playerEnterVehicle", (player, vehicle, seat) => {    
     mp.game.vehicle.defaultEngineBehaviour = false;//enginBehabior, dass der Motor ausbleibt beim einsteigen.
+    vehicle.setSiren(vehicle.isSirenOn());
+    vehicle.setSirenSound(vehicle.isSirenSoundOn());
 });
 
 let seatbelt = false;
@@ -22,6 +23,10 @@ mp.events.add('Apply:SeatBelt', () => {
 });
 
 mp.events.add('playerLeaveVehicle', ()  => {
+
+    vehicle.setSiren(vehicle.isSirenOn());
+    vehicle.setSirenSound(vehicle.isSirenSoundOn());
+
    if(seatbelt){
     mp.game.graphics.notify('Du hast dich beim aussteigen abgeschnallt');
     seatbelt = false;
