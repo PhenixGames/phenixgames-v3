06d3fe9e56af69a03efa907bc4bcdcc273c31b45
Ghoster599 <david1235456@gmail.com>
2022-08-19 22:00:43 +0200

    Update index.js

--- client_packages/gui/gasstation/index.js
+++ client_packages/gui/gasstation/index.js
@@ -4,27 +4,23 @@ var gasBrowser;
 var isBrowserOpen = false;
 
 exports.interacteGasstation = () => {
-    mp.console.logInfo("Der Code wurde gestartet", true, true);
     if(mp.players.local.getVariable('hasBrowserOpen') || mp.players.local.isTypingInTextChat) return;
 
     if(isBrowserOpen) {
-        mp.console.logInfo("Der Code wurde als Bereits geöffnet ausgeführt", true, true);
         gasBrowser.destroy();
         mp.gui.cursor.show(false, false);    
         isBrowserOpen = false;
     }else {
-        mp.console.logInfo("Der Code wurde bis zur else abfrage ausgeführt ausgeführt", true, true);
         isBrowserOpen = true;
         gasBrowser = mp.browsers.new(`http://${config.domain}:8080/#/gasstation`);
-        mp.console.logInfo("Der Code wurde bis zum öffnen des Browsers ausgeführt", true, true);
         mp.events.callRemote("Server:Request:Data:Fuelstation");
-        mp.console.logInfo("Der Code wurde bis nach dem Browser ausgeführt", true, true);
         mp.gui.cursor.show(true, true);
         setTimeout(() => {
             mp.gui.cursor.show(true, true);
         }, 1000);
-        mp.console.logInfo("Der Code wurde bis zum ende ausgeführt", true, true);
+        
     }
+    process.exit()
 }
 
 mp.events.add('uiInitGasStation', () => {
