8ed59cc2a0429d62fab768dfba8b184f1abd8d03
Ghoster599 <david1235456@gmail.com>
2022-09-27 21:07:41 +0200

    Update events.js

--- packages/PhenixGames/Fuelstation/events.js
+++ packages/PhenixGames/Fuelstation/events.js
@@ -34,7 +34,7 @@ mp.events.add("Server:Request:Data:Fuelstation", async (player) => {
     var Benzinpreis = res[0].fuel_sell_price_b * res[0].business_profit_mp;
     var Dieselpreis = res[0].fuel_sell_price_d * res[0].business_profit_mp;
     var Fuelsationname = res[0].name;
-    var Cars = []; //Only the nearest 5 in range of 5[meter=?] of fuelstation
+    var Cars = [{name: "test", name:"test2"}]; //Only the nearest 5 in range of 5[meter=?] of fuelstation
     let items = {
         
         'name': Fuelsationname, 
