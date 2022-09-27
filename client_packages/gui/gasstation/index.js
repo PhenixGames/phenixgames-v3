9229c25fb3f8652261faf3116a9a3bbc792c70ad
Ghoster599 <david1235456@gmail.com>
2022-09-27 22:11:41 +0200

    Update index.js

--- client_packages/gui/gasstation/index.js
+++ client_packages/gui/gasstation/index.js
@@ -34,7 +34,7 @@ mp.events.add('uiInitGasStation', () => {
  }, {}, ...]
 */
 mp.events.add('Player:Init:Gasstation', (items) => {
-    gasBrowser.execute(`gui.gasstation.initGasStation("<3", ${items});`);
+    gasBrowser.execute(`gui.gasstation.initGasStation("${items}");`);
 });
 
 mp.events.add('carRefuel', (carId, carFuel, price) => {
