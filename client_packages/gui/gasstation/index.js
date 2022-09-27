48f4581d8d7e66336af596a61c66f7cba87b3092
Mittelblut9 <sauer.benedikt@outlook.de>
2022-09-27 22:28:54 +0200

    debug g

--- client_packages/gui/gasstation/index.js
+++ client_packages/gui/gasstation/index.js
@@ -34,7 +34,7 @@ mp.events.add('uiInitGasStation', () => {
  }, {}, ...]
 */
 mp.events.add('Player:Init:Gasstation', (items) => {
-    mp.console.logInfo(JSON.stringify(items)), true, true); // When pressing F11, you should now see a message saying "example"
+    mp.console.logInfo(JSON.stringify(items), true, true); // When pressing F11, you should now see a message saying "example"
     gasBrowser.execute(`gui.gasstation.initGasStation("${[items]}");`);
 });
 
