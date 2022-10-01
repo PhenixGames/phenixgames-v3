ca0a68ddc76225442c30d4935518930e369d758a
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-01 02:10:24 +0200

    #47 Not found event hinzugefuegt

--- pg_vue_server/src/components/gasstation/Gasstation.component.vue
+++ pg_vue_server/src/components/gasstation/Gasstation.component.vue
@@ -1,57 +1,65 @@
 <template>
   <div class="gasstation user-select-none">
-    <p class="center white bold big">{{ gasstation }}</p>
-    <div class="gs_types">
-      <div class="gs_button gs_buttons_types cursor-pointer left"
-        :style="(selected_type == 'diesel') ? 'background: rgb(13, 162, 68)': '' " @click='changeCurrentType("diesel")'>
-        <p class="center white bold pointer-events-none user-select-none">Diesel
-          <br />
+    <section class="gs_notfound center white" v-if="notFound">
+      <h1>404</h1>
+      <h3>Diese Tankstelle wurde nicht gefunden</h3>
+      <h6 class="red">Bitte kontaktiere den Support!</h6>
+    </section>
+    <section v-else>
+      <p class="center white bold big">{{ gasstation }}</p>
+      <div class="gs_types">
+        <div class="gs_button gs_buttons_types cursor-pointer left"
+          :style="(selected_type == 'diesel') ? 'background: rgb(13, 162, 68)': '' "
+          @click='changeCurrentType("diesel")'>
+          <p class="center white bold pointer-events-none user-select-none">Diesel
+            <br />
 
-          <span class="white">1L / {{ diesel }}$</span>
-        </p>
-      </div>
-      <div class="gs_button gs_buttons_types cursor-pointer left"
-        :style="(selected_type == 'benzin') ? 'background: rgb(13, 162, 68)': '' " @click='changeCurrentType("benzin")'>
-        <p class="center white bold pointer-events-none user-select-none">Benzin
-          <br />
-          <span class="white">1L / {{ benzin }}$</span>
-        </p>
-      </div>
-    </div>
-
-    <div class="gs_cars">
-      <p class="center white">Fahrzeuge in der Nähe</p>
-      <div class="gs_cars_near">
-        <p v-if="cars.length === 0" class="center bold">Keine Fahrzeuge in der Nähe</p>
-        <div class="gs_button gs_buttons_cars cursor-pointer left" v-for="car in cars" :data-carid="car.id"
-          :data-max="car.max" :data-fuel_type="car.fuel_type"
-          :style="(selected_type !== car.fuel_type) ? 'cursor: not-allowed' : ''">
-          <p class="center white bold">{{car.name}}</p>
+            <span class="white">1L / {{ diesel }}$</span>
+          </p>
+        </div>
+        <div class="gs_button gs_buttons_types cursor-pointer left"
+          :style="(selected_type == 'benzin') ? 'background: rgb(13, 162, 68)': '' "
+          @click='changeCurrentType("benzin")'>
+          <p class="center white bold pointer-events-none user-select-none">Benzin
+            <br />
+            <span class="white">1L / {{ benzin }}$</span>
+          </p>
         </div>
       </div>
-    </div>
 
-    <hr>
-
-    <div class="gs_management display-grid justify-content-center">
-      <div class="gs_button cursor-pointer left">
-        <p class="center white bold">Management</p>
+      <div class="gs_cars">
+        <p class="center white">Fahrzeuge in der Nähe</p>
+        <div class="gs_cars_near">
+          <p v-if="cars.length === 0" class="center bold">Keine Fahrzeuge in der Nähe</p>
+          <div class="gs_button gs_buttons_cars cursor-pointer left" v-for="car in cars" :data-carid="car.id"
+            :data-max="car.max" :data-fuel_type="car.fuel_type"
+            :style="(selected_type !== car.fuel_type) ? 'cursor: not-allowed' : ''">
+            <p class="center white bold">{{car.name}}</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
 
+      <hr>
 
-  <div class="gs_fuel">
-    <span class="absolute white">0L</span>
-    <span class="absolute white">{{ car_max }}L</span>
-    <div class="gs_fuel_range" :style="{ '--min': 0, '--max': car_max, '--val': 0 }">
-      <input type="range" id="r" value="0" />
-      <output for="r" class="white">0L</output>
-    </div>
-    <span class="total_price center bold white">{{ current_price }}$</span>
-    <button class="gs_button gs_button_fuel cursor-pointer left">
-      <p class="center white bold" @click="car_refuel()">Tanken</p>
-    </button>
+      <div class="gs_management display-grid justify-content-center">
+        <div class="gs_button cursor-pointer left">
+          <p class="center white bold">Management</p>
+        </div>
+      </div>
+
+      <div class="gs_fuel">
+        <span class="absolute white">0L</span>
+        <span class="absolute white">{{ car_max }}L</span>
+        <div class="gs_fuel_range" :style="{ '--min': 0, '--max': car_max, '--val': 0 }">
+          <input type="range" id="r" value="0" />
+          <output for="r" class="white">0L</output>
+        </div>
+        <span class="total_price center bold white">{{ current_price }}$</span>
+        <button class="gs_button gs_button_fuel cursor-pointer left">
+          <p class="center white bold" @click="car_refuel()">Tanken</p>
+        </button>
+      </div>
+    </section>
   </div>
 
 
@@ -73,7 +81,8 @@ export default {
       cars: [],
       car: "0", // VEH ID
       car_max: 0, // VEH MAX FUEL
-      car_fuel_type: "", // VEH FUEL TYPE
+      car_fuel_type: "", // VEH FUEL TYPE,
+      notFound: false,
     };
   },
   methods: {
@@ -94,6 +103,9 @@ export default {
     },
     initComponent() {
       gui.gasstation = this;
+    },
+    gasstationNotFound() {
+      return (this.notFound = true);
     }
   },
   mounted() {
@@ -151,7 +163,7 @@ export default {
     });
 
     this.initComponent();
-    mp.trigger('uiInitGasStation');
+    //mp.trigger('uiInitGasStation');
   }
 };
 </script>
