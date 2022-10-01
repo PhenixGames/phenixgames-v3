3a2064b119037f0dd4fb91024b30bd701a93f251
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-01 22:51:18 +0200

    Code Refactor

--- packages/PhenixGames/playerDeath/index.js
+++ packages/PhenixGames/playerDeath/index.js
@@ -1,11 +1,13 @@
 const debug = require('../../../_assets/json/debug/debug.json').playerdeath;
 
+const cityHospital = new mp.Vector3(342.44623, -1398.0957, 32.50924)
+
 mp.events.add("playerDeath", (player, reason, killer) => {
     player.call("Open:Death:Browser");
 });
 
  mp.events.add("Respawn:At:Hospital", (player) => {
-    player.spawn(new mp.Vector3(342.44623, -1398.0957, 32.50924));
+    player.spawn(cityHospital);
     player.notify("~g~Du wurdest Respawnt")
     player.call('close:Death:Browser')
 });
