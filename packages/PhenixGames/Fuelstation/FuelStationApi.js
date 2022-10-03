4a803c01323693103736592e25d24074c1027deb
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-03 16:58:38 +0200

    testing

--- packages/PhenixGames/Fuelstation/FuelStationApi.js
+++ packages/PhenixGames/Fuelstation/FuelStationApi.js
@@ -135,7 +135,7 @@ class FuelStationApi {
         const returnVehicles = [];
         mp.vehicles.forEachInRange(player.position, 100,
             (vehicle) => {
-                console.log(vehicle);
+                console.log('Vehicles',vehicle);
                 if(returnVehicles.length < amount_of_vehicles){
                     returnVehicles.push(vehicle);
                 }else {
@@ -143,10 +143,6 @@ class FuelStationApi {
                 }
             }
         );
-	
-        if(debug){
-            console.log("Vehicles: " + JSON.stringify(returnVehicles));
-        }
 	    return returnVehicles;
     }
 
