b3417322068a823c9de201869dc05c906acab200
Ghoster599 <david1235456@gmail.com>
2022-10-03 13:30:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [$1 == "Start" ]
+then
+echo "Start script started"
 cd ./pg_vue_server
 pm2 start "npm run serve" --name rage_vue
 echo "vue server started"
@@ -7,3 +10,53 @@ pm2 logs
 sleep 3
 pm2 start ragemp-server --name rage_server
 echo "rage server started"
+fi
+if [$1 == "kill" ]
+then
+echo "Kill script Gestartet"
+pm2 stop rage_server
+pm2 stop rage_vue
+echo "Server stopped"
+pm2 delete rage_server
+pm2 delete rage_vue
+echo "Server Deleted"
+fi
+if [$1 == "pull" ]
+then
+pm2 stop rage_server
+pm2 stop rage_vue
+echo "Server Ausgeschaltet"
+pm2 delete rage_server
+pm2 delete rage_vue
+echo "Server Deleted"
+git pull
+sleep .5
+cd ./pg_vue_server
+pm2 start "npm run serve" --name rage_vue
+echo "vue server started"
+cd ..
+pm2 logs
+sleep 3
+pm2 start ragemp-server --name rage_server
+echo "rage server started"
+fi
+if [$1 == "Restart" ]
+then
+echo "Restart script started"
+pm2 stop rage_server
+pm2 stop rage_vue
+echo "Server stopped"
+pm2 delete rage_server
+pm2 delete rage_vue
+echo "Server Deleted"
+echo "Starting Restart"
+cd ./pg_vue_server
+pm2 start "npm run serve" --name rage_vue
+echo "vue server started"
+cd ..
+pm2 logs
+sleep 3
+pm2 start ragemp-server --name rage_server
+echo "rage server started"
+fi
+echo "script by David"
