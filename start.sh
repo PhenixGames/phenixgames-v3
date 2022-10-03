87eb674c71d3805b347671222841ad2ee150b2c8
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-03 13:19:23 +0200

    bug fix

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
 cd ./pg_vue_server
-pm2 start \"npm run serve\" --name rage_vue
+pm2 start "npm run serve" --name rage_vue
+echo "vue server started"
 cd ..
 pm2 logs
 pm2 start ragemp-server --name rage_server
+echo "rage server started"
