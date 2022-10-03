fc64cc325964fdf78048c110582c8202f1d5a1a3
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-03 15:26:19 +0200

    debug

--- packages/PhenixGames/Fuelstation/FuelStationApi.js
+++ packages/PhenixGames/Fuelstation/FuelStationApi.js
@@ -132,7 +132,7 @@ class FuelStationApi {
     }
 
     getClosestVehicles(player, range = 1) {
-        return mp.vehicles.getClosest([player.position.x, player.position.y, player.position.z], range)[0];
+        return mp.vehicles.getClosest([player.position.x, player.position.y, player.position.z], range);
     }
 
     fuel(player, fuel) {
