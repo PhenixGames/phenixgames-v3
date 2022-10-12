e95c79ebc1dabe3653643f0973faf201bae00b0b
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-12 22:36:06 +0200

    Code cleanup

--- client_packages/non-gui/player/keypresser.js
+++ client_packages/non-gui/player/keypresser.js
@@ -24,10 +24,7 @@ mp.keys.bind(0x58, true, function() {
 
 // -E-
 mp.keys.bind(0x45, true, function() {
-    if(mp.players.local.getVariable("isnearFuelstation")) {
-        if(mp.players.local.vehicle) {
-            return player.notify("~r~Du kannst nicht in einem Fahrzeug sein!");
-        }
+    if(mp.players.local.getVariable("isnearFuelstation") && !mp.players.local.vehicle) {    
         return interacteGasstation();
     }
 });
