e75961bbe08965526d103ed83d84967af6aad10d
Ghoster599 <david1235456@gmail.com>
2023-03-12 17:26:23 +0100

    Update PlayerLeft.js

--- client_packages/gui/HUD/PlayerLeft.js
+++ client_packages/gui/HUD/PlayerLeft.js
@@ -2,8 +2,7 @@ let leftarray = [];
 const drawtext = 'Spieler hat den Server Verlassen';
 let item;
 mp.events.add('Player:Quit:Lable', (drawpos) => {
-    //item = [Number(drawpos.x), Number(drawpos.y), Number(drawpos.z)];
-    item = [drawpos]
+    item = [Number(drawpos.x), Number(drawpos.y), Number(drawpos.z)];
     leftarray.push(item);
     setTimeout(() => {
         leftarray = leftarray.filter((i) => i !== item);
@@ -13,8 +12,7 @@ mp.events.add('Player:Quit:Lable', (drawpos) => {
 });
 mp.events.add('render', () => {
     leftarray.map((item) => {
-        //const pos = new mp.Vector3(item[0], item[1], item[2])
-        mp.game.graphics.drawText(drawtext, item[0], {
+        mp.game.graphics.drawText(drawtext, [item[0], item[1], item[2]], {
             font: 0,
             color: [255, 255, 255, 185],
             scale: [0.5,0.5],
