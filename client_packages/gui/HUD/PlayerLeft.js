468e5d02ebd7f9920be048d315b0be6aa2884120
Ghoster599 <david1235456@gmail.com>
2023-03-13 14:25:04 +0100

    Made better Text readability

--- client_packages/gui/HUD/PlayerLeft.js
+++ client_packages/gui/HUD/PlayerLeft.js
@@ -1,18 +1,18 @@
+//Declar Var
 let leftarray = [];
 const drawtext = '~y~Spieler hat\n den Server Verlassen';
 let item;
+//Code
 mp.events.add('Player:Quit:Lable', (drawpos) => {
-    item = [Number(drawpos.x), Number(drawpos.y), Number(drawpos.z)];
-    leftarray.push(item);
+    item = [Number(drawpos.x), Number(drawpos.y), Number(drawpos.z)];//Item erstellen
+    leftarray.push(item);//Item dem Array Hinzufügen
     setTimeout(() => {
-        leftarray = leftarray.filter((i) => i !== item);
+        leftarray = leftarray.filter((i) => i !== item);//Item nach 10 Sekunden Entfernen
     }, 10000); //10 Sekunden
-    mp.console.logInfo("Drawpos: " + drawpos, true, true);
-    mp.console.logInfo("item: " + item, true, true);
 });
-mp.events.add('render', () => {
-    leftarray.map((item) => {
-        mp.game.graphics.drawText(drawtext, [item[0], item[1], item[2]+1], {
+mp.events.add('render', () => {//Render Funktion wird jeden Frame Ausgeführt
+    leftarray.map((item) => {//Mappe durch den Array an Leftplayers
+        mp.game.graphics.drawText(drawtext, [item[0], item[1], item[2]+1], {//Zeichne Den text in die Welt
             font: 0,
             color: [255, 255, 255, 185],
             scale: [0.5,0.5],
--- packages/PhenixGames/account/playerQuit.js
+++ packages/PhenixGames/account/playerQuit.js
@@ -1,9 +1,8 @@
 const AccountAPI = require('./AcountAPI');
 
 mp.events.add('playerQuit', (player) => {
-    AccountAPI.updatePlayerOnline();
-
-    mp.players.forEachInRange(player.position, 200, (target) => {
-        target.call('Player:Quit:Lable', [player.position]);
+    AccountAPI.updatePlayerOnline();//Update var
+    mp.players.forEachInRange(player.position, 200, (target) => {//Get all Players in range of Leaving Player
+        target.call('Player:Quit:Lable', [player.position]);//Call Event on Player
     });
 });
